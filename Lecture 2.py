14958e63e5af77b44dc08eb63a017aff0620be84
Atharv Mule <atharvamule8055@gmail.com>
2025-07-07 19:05:17 +0530

    Add files via upload

---
+++ Lecture 2.py
@@ -0,0 +1,12 @@
+# #We will learn hot change the sentance line in any string
+# str1="This is a string. \nWe are creating in python." #We used '\n' to break line and go to second line
+# print(str1)
+
+
+#Adding two strings
+First_Name="Atharv"
+Second_Name="Mule"
+Full_Name=First_Name+Second_Name
+print(Full_Name)
+Full_Name_with_space=First_Name+" "+Second_Name
+print(Full_Name_with_space)
