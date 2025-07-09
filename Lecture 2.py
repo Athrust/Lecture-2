ce9394673d673b93d3bb9c5ef48c64a1443d5ba0
Atharv Mule <atharvamule8055@gmail.com>
2025-07-09 19:09:02 +0530

    modified lecture 2

--- Lecture 2.py
+++ Lecture 2.py
@@ -3,14 +3,25 @@
 # print(str1)
 
 
-#Adding two strings
-First_Name="Atharv"
-Second_Name="Mule"
-Full_Name=First_Name+Second_Name
-print(Full_Name)
-Full_Name_with_space=First_Name+" "+Second_Name
-print(Full_Name_with_space)
-num1=23
-num2=27
-print(num1+num2)
-print("CHECK")
+# #Adding two strings
+# First_Name="Atharv"
+# Second_Name="Mule"
+# Full_Name=First_Name+Second_Name
+# print(Full_Name)
+# Full_Name_with_space=First_Name+" "+Second_Name
+# print(Full_Name_with_space)
+# print(len(Full_Name_with_space)) #This gives the number of character present in the given string(11)
+
+
+
+
+#Indexing
+str="Atharv Mule"
+print(str[0])
+print(str[1])
+print(str[2])
+print(str[3])
+print(str[4])
+print(str[5])
+print(str[6]) #Here we can the n'th number character in given stting with "[]" as shown
+print(str[1:5])
