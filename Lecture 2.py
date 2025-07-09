9a2bf66cf324a0a5832edea22171bcdd628a259d
Atharv Mule <atharvamule8055@gmail.com>
2025-07-09 19:21:26 +0530

    Updated second lecture

--- Lecture 2.py
+++ Lecture 2.py
@@ -24,4 +24,13 @@ print(str[3])
 print(str[4])
 print(str[5])
 print(str[6]) #Here we can the n'th number character in given stting with "[]" as shown
-print(str[1:5])
+print(str[1:5]) #Gives the character from n'th position to n'th position
+print (str[:4]) #[0:4]
+print(str[5:]) #[5:len(str)]
+print(str[-6:-1])
+print(str[-1])
+print(str[-2])
+print(str[-3])
+print(str[-4])
+print(str[-5])
+print(str[-6])
